f07e5b00ddb66afb6ce77c010ea7bfb0fae65490
DmitryBal <76563919+DmitryBal@users.noreply.github.com>
2022-12-14 17:09:54 +0300

    Create hw.c

---
+++ hw.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "buz.h"
+
+int
+main(int argc, char **argv, char **envp) {
+        // int n = func1(argc);
+        int n = 42;
+        printf("Hello, %d!\n", n);
+        return 0;
+}
+
